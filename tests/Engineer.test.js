7c5965d3f05187718a93422d5dda60810efe7952
Brett Hicklin <hicklin.brett@gmail.com>
2023-03-11 19:05:09 -0500

    completed unit testing on Engineers

---
+++ tests/Engineer.test.js
@@ -0,0 +1,21 @@
+const Engineer = require("../lib/Engineer.js")
+
+describe('Engineer',() =>{
+    describe('getGithub',()=>{
+        test('it should return the Github username',()=>{
+            const engineer = new Engineer("brett","03","test@test.com","brett-hicklin");
+            const github = engineer.getGithub();
+            expect(github).toBe("brett-hicklin");
+        })
+    })
+
+    describe('getRole',()=>{
+        test('it should return the string "Engineer"',()=>{
+            const engineer = new Engineer("brett","02","test@test.com","brett-hicklin");
+            const role = engineer.getRole();
+            expect(role).toBe("Engineer");
+
+        })
+
+    })
+})
